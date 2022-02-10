63608024a4eb5bb8e9aa8f230e781e5cf34bdf4d
rob mathews <rob.mathews@goodmeasures.com>
2022-02-10 15:10:42 -0500

    delete badly named and non-existant queues

--- environments/production/queues/ZZZ_Ignore_01.tf
+++
@@ -1,8 +0,0 @@
-          resource "aws_connect_queue" "zzz_ignore_01" {
-            description  = "A"
-            hours_of_operation_id = aws_connect_hours_of_operation.basic_hours.hours_of_operation_id
-            instance_id  = aws_connect_instance.connect.id
-            name         = "ZZZ - Ignore 01"
-            }
-            tags = local.tags
-          }
