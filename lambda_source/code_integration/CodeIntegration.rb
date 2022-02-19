9b709d677b88c05f510fa33a2c8722a735c72047
rob mathews <rob.mathews@goodmeasures.com>
2022-02-19 04:47:22 -0500

    Removed dependency on httparty  only depends on standard libraries now. No need to bundle install

--- lambda_source/code_integration/CodeIntegration.rb
+++ lambda_source/code_integration/CodeIntegration.rb
@@ -1,30 +1,50 @@
-require 'json/pure'
-require 'httparty'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'date'
 class CodeIntegration
-    include HTTParty
-
-    def self.handle_human_not_answered(event:, context:)
-        puts "hello world"
+    def self.record_interaction(event:, context:)
         puts JSON.pretty_generate(event)
         attributes = event["Details"]["ContactData"]["Attributes"]
-        post_body = {
-            user_id: attributes["UserId"],
-            reason_code_id: attributes["ReasonCodeId"],
-            encounter_date: Date.today,
-            coach_id: attributes["CoachId"],
-            channel: "phone",
-            time_spent: 1,
-            additional_time_spent: 1,
-            interaction_notes: "User did not answer the phone. Left voicemail.",
-            subject: "Automated Appointment Reminder",
-            appointment_id: attributes["AppointmentId"],
+        parameters = event["Details"]["Parameters"]
+        body = {
+            engagement:
+            {
+                user_id: attributes["UserId"],
+                reason_code_id: attributes["ReasonCodeId"],
+                encounter_date: Date.today,
+                coach_id: attributes["CoachId"],
+                channel: "phone",
+                time_spent: 1,
+                additional_time_spent: 1,
+                interaction_notes: parameters["InteractionNotes"],
+                subject: parameters["Subject"],
+                appointment_id: attributes["AppointmentId"],
+                reference_number:event["Details"]["ContactData"]["ContactId"]
+            }
         }
 
-        auth = {
-            username: ENV["username"],
-            password: ENV["password"]
-        }
-        
-        post("#{attributes["Hostname"]}/api/engagement.json", :body => post_body.to_json, basic_auth: auth)
+        uri = URI.parse("#{attributes["Hostname"]}/api/engagement.json")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.body = body.to_json
+        request.basic_auth ENV["username"], ENV["password"]
+        response = http.request(request)
     end
 end
+
+# ENV["username"] = "user-1"
+# ENV["password"] = "stagingPassword123!"
+# CodeIntegration.record_interaction(event:
+#  {"Details" => {
+#     "ContactData" => {
+#         "Attributes" => {
+#             # "Hostname" => "http://localhost:3000"
+#             "Hostname" => "https://77d6-130-44-150-166.ngrok.io"
+#         },
+#         "ContactId" => "123"
+#     }
+# }},
+# context: {}
+# )
--- lambda_source/code_integration/build.sh
+++ lambda_source/code_integration/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf vendor
-bundle config set --local path 'vendor/bundle'
-bundle install
+# rm -rf vendor
+# bundle config set --local path 'vendor/bundle'
+# bundle install
 zip -r ../../compiled/code_integration .
